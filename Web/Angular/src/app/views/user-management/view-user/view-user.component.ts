3e9061f33175c82ca8656aa810bc1ff1e46e9360
wupinbobo <lamtdse61743@fpt.edu.vn>
2018-02-12 11:44:33 +0700

    chinh sua trang view cua user
    <3

--- Web/Angular/src/app/views/user-management/view-user/view-user.component.ts
+++ Web/Angular/src/app/views/user-management/view-user/view-user.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import {UserService} from '../../../services/user.service';
+import { User } from 'app/interfaces/user';
 @Component({
   selector: 'app-view-user',
   templateUrl: './view-user.component.html',
   styleUrls: ['./view-user.component.scss']
 })
 export class ViewUserComponent implements OnInit {
-
-  constructor() { }
+  users: User[];
+  constructor(private userService:UserService) { }
 
   ngOnInit() {
+    this.userService.getAllUser()
+    .then(value =>
+      this.users = value
+    )
   }
 
 }
