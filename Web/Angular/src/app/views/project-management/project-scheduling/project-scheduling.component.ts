fed8cf92d355736155b023adc06b2fcda9c9446e
loiphong1996 <tranloiphong2@gmail.com>
2018-02-26 20:57:23 +0700

    Add gantt dependency

--- Web/Angular/src/app/views/project-management/project-scheduling/project-scheduling.component.ts
+++ Web/Angular/src/app/views/project-management/project-scheduling/project-scheduling.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import 'dhtmlx-gantt';
+import {} from '@types/dhtmlxgantt';
 
 @Component({
   selector: 'app-project-scheduling',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-scheduling.component.scss']
 })
 export class ProjectSchedulingComponent implements OnInit {
+  @ViewChild('gantt_here') ganttContainer: ElementRef;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    gantt.config.xml_date = '%Y-%m-%d %H:%i';
+    gantt.init(this.ganttContainer.nativeElement);
+    // gantt.parse()
   }
 
 }
