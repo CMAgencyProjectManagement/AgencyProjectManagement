17f5ea5c4e55e9f848936ad8254e8e9ca594459f
loiphong1996 <tranloiphong2@gmail.com>
2018-02-27 12:56:55 +0700

    Add more detail to get my project API

--- Service/ListService.cs
+++ Service/ListService.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entity;
+using Newtonsoft.Json.Linq;
 
 namespace Service
 {
-    class ListService
+    static class ListService
     {
+        public static JObject ToJson(this List list, bool isDetailed = true)
+        {
+            JObject result = new JObject
+            {
+                ["ID"] = list.ID,
+                ["Name"] = list.Name
+            };
+            
+            if (isDetailed)
+            {
+                JArray tasksJArray = new JArray();
+
+                foreach (var task in list.Tasks)
+                {
+                    tasksJArray.Add(task.ToJson());
+                }
+
+                result["tasks"] = tasksJArray;
+            }
+
+            return result;
+        }
     }
 }
--- Service/TaskService.cs
+++ Service/TaskService.cs
@@ -1,12 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Entity;
+using Newtonsoft.Json.Linq;
 
 namespace Service
 {
-    class TaskService
+    static class TaskService
     {
+        public static List<Task> GetTasksOfProject(int projectId)
+        {
+            using (CmAgencyEntities entities = new CmAgencyEntities())
+            {
+                Project project = entities.Projects.Find(projectId);
+                if (project != null)
+                {
+                    var taskList = new List<Task>();
+                    foreach (var list in project.Lists)
+                    {
+                        foreach (var task in list.Tasks)
+                        {
+                            taskList.Add(task);
+                        }
+                    }
+
+                    return taskList;
+                }
+                else
+                {
+                    throw new ObjectNotFoundException($"Project with ID {projectId} not found");
+                }
+            }
+        }
+
+        public static JObject ToJson(this Task task)
+        {
+            JObject result = new JObject
+            {
+                ["ID"] = task.ID,
+                ["Name"] = task.Name,
+                ["Description"] = task.Description,
+                ["CreatedBy"] = task.CreatedBy,
+                ["CreatedTime"] = task.CreatedTime,
+                ["ChangedBy"] = task.ChangedBy,
+                ["ChangedTime"] = task.ChangedTime,
+                ["Status"] = task.Status,
+                ["Duration"] = task.Duration,
+                ["ListID"] = task.ListID,
+                ["Priority"] = task.Priority,
+                ["StartDate"] = task.StartDate
+            };
+
+            return result;
+        }
     }
 }
