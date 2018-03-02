d51cf3d88a5bfc05bc8e9ae1762190e215e28564
wupinbobo <lamtdse61743@fpt.edu.vn>
2018-03-02 11:53:33 +0700

    Fix View User

--- Web/Angular/src/app/views/user-management/create-user/create-user.component.ts
+++ Web/Angular/src/app/views/user-management/create-user/create-user.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  OnInit
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { UserService } from '../../../services/user.service';
+import { Cursor, StoreService } from '../../../services/tree.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-user',
@@ -6,10 +19,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-user.component.scss']
 })
 export class CreateUserComponent implements OnInit {
+  signupForm: FormGroup;
+  currentAccountCursor: Cursor;
+  tokenCursor: Cursor;
+  isLoading: boolean;
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private accountHub: UserService,
+    private storeService: StoreService,
+    private router: Router) {
+    this.currentAccountCursor = this.storeService.select(['currentUser']);
+    this.tokenCursor = this.storeService.select(['token']);
+    this.isLoading = false;
+  }
 
   ngOnInit() {
+    this.signupForm = new FormGroup({
+      username: new FormControl(undefined, Validators.required),
+      password: new FormControl(undefined, Validators.required),
+      fullname: new FormControl(undefined, Validators.required),
+      email: new FormControl(undefined, Validators.required),
+      gender: new FormControl(undefined, Validators.required),
+      avatar: new FormControl(undefined, Validators.required)
+    })
+  }
+
+  handleEnterPressed($event) {
+    if ($event.keyCode === 13) {
+      this.handleLogin();
+    }
+  }
+
+  handleLogin() {
+    if (this.signupForm.valid) {
+      const formValue = this.signupForm.value;
+      this.isLoading = true;
+      console.log(this.isLoading);
+      this.accountHub.login(
+        formValue.username,
+        formValue.password,
+      ).then(value => {
+        this.isLoading = false;
+        this.router.navigate(['dashboard'])
+      }).catch(reason => {
+        this.isLoading = false;
+        this.errorMessage = reason.message;
+      })
+    }
   }
 
 }
