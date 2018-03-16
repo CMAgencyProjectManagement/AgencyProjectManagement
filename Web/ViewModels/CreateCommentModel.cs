b272254a7001c86de13b5b980be14710ffc21fe6
Pham Thanh Tung <tungptse61628@fpt.edu.vn>
2018-03-16 16:01:55 +0700

    add comment to task

---
+++ Web/ViewModels/CreateCommentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web.ViewModels
+{
+    public class CreateCommentModel
+    {
+        public String Body { get; set; }
+
+        [Required(ErrorMessage = "TaskID must not be empty")]
+        public int TaskID { get; set; }
+    }
+}
