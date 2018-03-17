16e9bb1649feea253c11b8d1bc2d3d3cbae679ec
Điền <diendtlse61501@fpt.edu.vn>
2018-03-17 17:46:31 +0700

    thêm tên user

--- Web/Angular/src/app/views/user-management/detail-user/detail-user.component.ts
+++ Web/Angular/src/app/views/user-management/detail-user/detail-user.component.ts
@@ -1,15 +1,76 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TeamService } from '../../../services/team.service';
+import { UserService } from '../../../services/user.service';
+import { Team } from 'app/interfaces/team';
+import { User } from 'app/interfaces/user';
 @Component({
   selector: 'app-detail-user',
   templateUrl: './detail-user.component.html',
   styleUrls: ['./detail-user.component.scss']
 })
 export class DetailUserComponent implements OnInit {
+  entity: any;
+  userID: number;
+  teams: Team[];
+  users: User[];
 
-  constructor() { }
+  foundUser: User;
+  selectedUser = [];
+  isLoading: boolean;
+  dropdownSettings = {};
 
+  constructor(
+    private teamService: TeamService,
+    private userService: UserService,
+  ) { }
   ngOnInit() {
+
+    
+    this.entity = {};
+    this.userID = Number(this.GetURLParameter('id'));
+    this.teams = [];
+    this.users = [];
+    this.getAllTeam();
+
+
+  }
+  getAllTeam() {
+    this.userService.getAllUser()
+      .then(value => {
+        this.users = value;
+        for (let i = 0; i < this.users.length; i++) {
+          if (this.users[i].id == this.userID) {
+            this.foundUser = this.users[i];
+            this.selectedUser.push({
+              id: this.foundUser.id,
+              itemName: this.foundUser.username
+            });
+          }
+        }
+      })
   }
 
+  GetURLParameter(sParam) {
+    var sPageURL = window.location.href;
+    var sURLVariables = sPageURL.split('?');
+    var sTeam = sURLVariables[1].split('=');
+    return sTeam[1];
+  }
+
+  onItemSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedUser);
+  }
+  OnItemDeSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedUser);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
+  }
+  onDeSelectAll(items: any) {
+    console.log(items);
+  }
+  submitChange() {
+  }
 }
