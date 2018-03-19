fa29918428b4793539850f8ee941ef534e148e73
phongtlse61770 <phongtlse61770@fpt.edu.vn>
2018-03-19 20:35:54 +0700

    fix bug in account management search bar

--- Web/Angular/src/app/views/user-management/view-user/view-user.component.ts
+++ Web/Angular/src/app/views/user-management/view-user/view-user.component.ts
@@ -3,6 +3,7 @@ import {UserService} from '../../../services/user.service';
 import {User} from 'app/interfaces/user';
 import * as _ from 'lodash';
 import * as moment from 'moment';
+import {DataTableDirective} from 'angular-datatables';
 
 @Component({
   selector: 'app-view-user',
@@ -15,8 +16,9 @@ export class ViewUserComponent implements OnInit {
   users: User[];
   // https://datatables.net/reference/option/
   isPageLoading: boolean;
+  @ViewChild(DataTableDirective) datatableElement: DataTableDirective;
   datatableOptions: DataTables.Settings = {
-    searching: false,
+    searching: true,
     lengthChange: false,
     columnDefs: [
       {
@@ -43,55 +45,57 @@ export class ViewUserComponent implements OnInit {
 
   // search by username
   search(searchStr: string) {
-    this.isPageLoading = true;
-    this.userService.getAllUser()
-      .then(users => {
-        this.users = _.filter(users, (user: User) => {
-            let result;
-            result = user.name && _.toLower(user.name).indexOf(_.toLower(searchStr)) >= 0;
-
-            if (!result) {
-              result = user.username && _.toLower(user.username).indexOf(_.toLower(searchStr)) >= 0;
-            }
-
-            if (!result) {
-              if (user.birthdate) {
-                let formatedBDate = moment(user.birthdate).format('DD/MM/YYYY');
-                result = _.toLower(formatedBDate).indexOf(_.toLower(searchStr)) >= 0;
-              }
-            }
-
-            if (!result) {
-              result = user.team && _.toLower(user.team.name).indexOf(_.toLower(searchStr)) >= 0;
-            }
-
-            if (!result) {
-              if (user.isAdmin) {
-                result = _.toLower('Admin').indexOf(_.toLower(searchStr)) >= 0;
-              } else if (user.isManager) {
-                result = _.toLower('Manager').indexOf(_.toLower(searchStr)) >= 0;
-              } else {
-                result = _.toLower('Staff').indexOf(_.toLower(searchStr)) >= 0;
-              }
-            }
-
-            if (!result) {
-              if (user.isActive) {
-                result = _.toLower('Active').indexOf(_.toLower(searchStr)) >= 0;
-              } else {
-                result = _.toLower('Banned').indexOf(_.toLower(searchStr)) >= 0;
-              }
-            }
-
-            return result;
-          }
-        );
-        this.isPageLoading = false;
-      })
-      .catch(reason => {
-        this.isPageLoading = false;
-      });
-
+    this.datatableElement.dtInstance.then(
+      (dtInstance: DataTables.Api) => dtInstance.search(searchStr).draw()
+    );
+    // this.isPageLoading = true;
+    // this.userService.getAllUser()
+    //   .then(users => {
+    //     this.users = _.filter(users, (user: User) => {
+    //         let result;
+    //         result = user.name && _.toLower(user.name).indexOf(_.toLower(searchStr)) >= 0;
+    //
+    //         if (!result) {
+    //           result = user.username && _.toLower(user.username).indexOf(_.toLower(searchStr)) >= 0;
+    //         }
+    //
+    //         if (!result) {
+    //           if (user.birthdate) {
+    //             let formatedBDate = moment(user.birthdate).format('DD/MM/YYYY');
+    //             result = _.toLower(formatedBDate).indexOf(_.toLower(searchStr)) >= 0;
+    //           }
+    //         }
+    //
+    //         if (!result) {
+    //           result = user.team && _.toLower(user.team.name).indexOf(_.toLower(searchStr)) >= 0;
+    //         }
+    //
+    //         if (!result) {
+    //           if (user.isAdmin) {
+    //             result = _.toLower('Admin').indexOf(_.toLower(searchStr)) >= 0;
+    //           } else if (user.isManager) {
+    //             result = _.toLower('Manager').indexOf(_.toLower(searchStr)) >= 0;
+    //           } else {
+    //             result = _.toLower('Staff').indexOf(_.toLower(searchStr)) >= 0;
+    //           }
+    //         }
+    //
+    //         if (!result) {
+    //           if (user.isActive) {
+    //             result = _.toLower('Active').indexOf(_.toLower(searchStr)) >= 0;
+    //           } else {
+    //             result = _.toLower('Banned').indexOf(_.toLower(searchStr)) >= 0;
+    //           }
+    //         }
+    //
+    //         return result;
+    //       }
+    //     );
+    //     this.isPageLoading = false;
+    //   })
+    //   .catch(reason => {
+    //     this.isPageLoading = false;
+    //   });
   }
 
 }
