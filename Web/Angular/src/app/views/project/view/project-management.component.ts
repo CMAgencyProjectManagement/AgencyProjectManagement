61fdb51edd26847a394afd1242ba2d51dbef4ebe
phongtlse61770 <phongtlse61770@fpt.edu.vn>
2018-03-19 20:31:08 +0700

    Update search bar on project management (fix bug)

--- Web/Angular/src/app/views/project/view/project-management.component.ts
+++ Web/Angular/src/app/views/project/view/project-management.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ProjectService} from '../../../services/project.service';
 import {Project} from '../../../interfaces/project';
 import * as _ from 'lodash';
 import * as moment from 'moment';
+import {DataTableDirective} from 'angular-datatables';
 
 @Component({
   selector: 'app-project-management',
@@ -14,8 +15,8 @@ export class ProjectManagementComponent implements OnInit {
   projects: Project[];
   isLoading: boolean;
   isPageLoading: boolean;
+  @ViewChild(DataTableDirective) datatableElement: DataTableDirective;
   datatableOptions: DataTables.Settings = {
-    searching: false,
     lengthChange: false,
     columnDefs: [
       {
@@ -31,10 +32,6 @@ export class ProjectManagementComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.loadProject();
-  }
-
-  loadProject() {
     this.projectService.getAllProjects()
       .then(data => {
         this.projects = data;
@@ -45,58 +42,46 @@ export class ProjectManagementComponent implements OnInit {
       })
   }
 
-  handleClose(projectID: number) {
-    console.debug(projectID);
-    this.projectService.closeProject(
-      projectID
-    ).then(value => {
-      this.isLoading = false;
-      this.loadProject();
-    }).catch(reason => {
-      this.isLoading = false;
-      console.debug(reason);
-      // this.handleCloseError(reason.Data);
-    })
-  }
-
   search(searchStr: string) {
-    this.isPageLoading = true;
-    this.projectService.getAllProjects()
-      .then(projects => {
-        this.projects = _.filter(projects, (project: Project) => {
-          let result = false;
-          if (project.name) {
-            result = _.toLower(project.name).indexOf(_.toLower(searchStr)) >= 0;
-
-          }
-
-          if (!result && project.description) {
-            result = project.description && _.toLower(project.description).indexOf(_.toLower(searchStr)) >= 0
-          }
-
-          if (!result && project.deadline) {
-            let formatted = moment(project.deadline).format('DD/MM/YYYY');
-            result = _.toLower(formatted).indexOf(_.toLower(searchStr)) >= 0;
-          }
-
-          if (!result && project.startDate) {
-            let formatted = moment(project.startDate).format('DD/MM/YYYY');
-            result = _.toLower(formatted).indexOf(_.toLower(searchStr)) >= 0;
-
-          }
-
-          if (!result && project.createdTime) {
-            let formatted = moment(project.createdTime).format('DD/MM/YYYY');
-            result = _.toLower(formatted).indexOf(_.toLower(searchStr)) >= 0;
-
-          }
-
-          return result;
-        });
-        this.isPageLoading = false;
-      })
-      .catch(reason => {
-        console.log('viewallproject--search ' + reason);
-      })
+    this.datatableElement.dtInstance.then(
+      (dtInstance: DataTables.Api) => dtInstance.search(searchStr).draw()
+    );
+    // this.projectService.getAllProjects()
+    //   .then(projects => {
+    //     this.projects = _.filter(projects, (project: Project) => {
+    //       let result = false;
+    //       if (project.name) {
+    //         result = _.toLower(project.name).indexOf(_.toLower(searchStr)) >= 0;
+    //
+    //       }
+    //
+    //       if (!result && project.description) {
+    //         result = project.description && _.toLower(project.description).indexOf(_.toLower(searchStr)) >= 0
+    //       }
+    //
+    //       if (!result && project.deadline) {
+    //         let formatted = moment(project.deadline).format('DD/MM/YYYY');
+    //         result = _.toLower(formatted).indexOf(_.toLower(searchStr)) >= 0;
+    //       }
+    //
+    //       if (!result && project.startDate) {
+    //         let formatted = moment(project.startDate).format('DD/MM/YYYY');
+    //         result = _.toLower(formatted).indexOf(_.toLower(searchStr)) >= 0;
+    //
+    //       }
+    //
+    //       if (!result && project.createdTime) {
+    //         let formatted = moment(project.createdTime).format('DD/MM/YYYY');
+    //         result = _.toLower(formatted).indexOf(_.toLower(searchStr)) >= 0;
+    //
+    //       }
+    //
+    //       return result;
+    //     });
+    //     this.isPageLoading = false;
+    //   })
+    //   .catch(reason => {
+    //     console.log('viewallproject--search ' + reason);
+    //   })
   }
 }
