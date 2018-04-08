c3587f14292bd736a4fb6f23d663de16a88d171e
phongtlse61770 <phongtlse61770@fpt.edu.vn>
2018-04-08 20:04:58 +0700

    Update db.sql and entity model

--- Entity/Task.cs
+++ Entity/Task.cs
@@ -38,6 +38,7 @@ namespace Entity
         public Nullable<int> Priority { get; set; }
         public Nullable<System.DateTime> StartDate { get; set; }
         public Nullable<System.DateTime> FinishedDate { get; set; }
+        public int Effort { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Attachment> Attachments { get; set; }
