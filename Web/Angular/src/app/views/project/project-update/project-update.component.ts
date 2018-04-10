029e75498f4b6e537a9baeda9e8e65f221eb1cf5
loiphong1996 <tranloiphong2@gmail.com>
2018-04-10 11:06:16 +0700

    Remove unsed Import

--- Web/Angular/src/app/views/project/project-update/project-update.component.ts
+++ Web/Angular/src/app/views/project/project-update/project-update.component.ts
@@ -2,25 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProjectService } from '../../../services/project.service';
 import { Project } from '../../../interfaces/project';
 import { Cursor, StoreService } from '../../../services/tree.service';
-import { ElementRef, ViewChild } from '@angular/core';
-import { UserService, } from '../../../services/user.service';
 import { PagerService } from '../../../services/pager.service';
-import { User } from 'app/interfaces/user';
-import { Pager } from '../../../interfaces/pager';
-import { Directive, HostListener, Input } from '@angular/core';
-import * as _ from 'lodash';
-import {
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
 import { Router } from '@angular/router';
-import { forEach } from '@angular/router/src/utils/collection';
-import { DISABLED } from '@angular/forms/src/model';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ModalDirective } from 'ngx-bootstrap/modal';
-// Modal Component
-import { ModalModule } from 'ngx-bootstrap/modal';
 @Component({
   selector: 'app-project-update',
   templateUrl: './project-update.component.html',
@@ -70,6 +53,5 @@ export class ProjectUpdateComponent implements OnInit {
       var sUsername = sURLVariables[1].split('=');
       return sUsername[1];
     }
-  
+
   }
-  
