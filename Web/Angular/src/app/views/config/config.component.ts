e59e2821a7554b59a15e7930363c23651c50b8ec
Điền <diendtlse61501@fpt.edu.vn>
2018-04-25 18:31:59 +0700

    Giao dien trang config

--- Web/Angular/src/app/views/config/config.component.ts
+++ Web/Angular/src/app/views/config/config.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+//import {ConfigService} from '../../../services/config.service';
+// import {ActivatedRoute, Router} from '@angular/router';
+// import * as moment from 'moment';
+// import {
+//   FormControl,
+//   FormGroup,
+// } from '@angular/forms';
+// import {IMyDpOptions} from 'mydatepicker';
+// import {BsModalService} from 'ngx-bootstrap/modal';
+
+// import * as _ from 'lodash';
 
 @Component({
   selector: 'app-config',
@@ -6,6 +17,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./config.component.scss']
 })
 export class ConfigComponent implements OnInit {
+  // isLoading: {
+  //   page: boolean,
+  //   create: boolean
+  // };
+
+  // errors: {
+  //   name: string,
+  //   description: string,
+  //   list: string,
+  //   priority: string,
+  //   startDate: string,
+  //   duration: string,
+  //   effort: string,
+  //   predecessors: string;
+  // };
 
   constructor() { }
 
