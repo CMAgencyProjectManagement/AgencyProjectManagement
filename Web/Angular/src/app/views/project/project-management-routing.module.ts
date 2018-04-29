f288e44cffb1b6370b609a8e5e3a38c0d4d4201c
Điền <diendtlse61501@fpt.edu.vn>
2018-04-29 11:16:00 +0700

    Chỉnh lại scrum project

--- Web/Angular/src/app/views/project/project-management-routing.module.ts
+++ Web/Angular/src/app/views/project/project-management-routing.module.ts
@@ -1,71 +1,79 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {ProjectManagementComponent} from './view/project-management.component';
-import {ProjectSchedulingComponent} from './project-scheduling/project-scheduling.component';
-import {ProjectDetailComponent} from './project-detail/project-detail.component';
-import {AddProjectComponent} from './add-project/add-project.component';
-import {ProjectUpdateComponent} from './project-update/project-update.component';
-import {ProjectTaskComponent} from './project-task/project-task.component';
-import {ReportComponent} from './report/report.component';
-import {ArchiveComponent} from './archive/archive.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProjectManagementComponent } from './view/project-management.component';
+import { ProjectSchedulingComponent } from './project-scheduling/project-scheduling.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { AddProjectComponent } from './add-project/add-project.component';
+import { ProjectUpdateComponent } from './project-update/project-update.component';
+import { ProjectTaskComponent } from './project-task/project-task.component';
+import { ReportComponent } from './report/report.component';
+import { ArchiveComponent } from './archive/archive.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: ProjectManagementComponent,
     data: {
-      title: 'View projects'
-    }
-  },
-  {
-    path: ':id/schedule',
-    component: ProjectSchedulingComponent,
-    data: {
-      title: 'Project schedule'
-    }
-  },
-  {
-    path: ':id/detail',
-    component: ProjectDetailComponent,
-    data: {
-      title: 'Project detail'
-    }
-  },
-  {
-    path: 'add',
-    component: AddProjectComponent,
-    data: {
-      title: 'Create project'
-    }
-  },
-  {
-    path: ':id/update',
-    component: ProjectUpdateComponent,
-    data: {
-      title: 'Update project'
-    }
-  },
-  {
-    path: ':id/task',
-    component: ProjectTaskComponent,
-    data: {
-      title: 'View tasks'
-    }
-  },
-  {
-    path: ':id/report',
-    component: ReportComponent,
-    data: {
-      title: 'View report'
-    }
-  },
-  {
-    path: ':id/archive',
-    component: ArchiveComponent,
-    data: {
-      title: 'View archives'
-    }
-  },
+      title: 'Project'
+    },
+    children: [
+      {
+        path: '',
+        component: ProjectManagementComponent,
+        data: {
+          title: 'View projects'
+        }
+      },
+      {
+        path: ':id/schedule',
+        component: ProjectSchedulingComponent,
+        data: {
+          title: 'Project schedule'
+        }
+      },
+      {
+        path: ':id/detail',
+        component: ProjectDetailComponent,
+        data: {
+          title: 'Project detail'
+        }
+      },
+      {
+        path: 'add',
+        component: AddProjectComponent,
+        data: {
+          title: 'Create project'
+        }
+      },
+      {
+        path: ':id/update',
+        component: ProjectUpdateComponent,
+        data: {
+          title: 'Update project'
+        }
+      },
+      {
+        path: ':id/task',
+        component: ProjectTaskComponent,
+        data: {
+          title: 'View tasks'
+        }
+      },
+      {
+        path: ':id/report',
+        component: ReportComponent,
+        data: {
+          title: 'View report'
+        }
+      },
+      {
+        path: ':id/archive',
+        component: ArchiveComponent,
+        data: {
+          title: 'View archives'
+        }
+      },
+    ]
+  }
 ];
 
 @NgModule({
