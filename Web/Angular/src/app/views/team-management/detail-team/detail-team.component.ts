fed1aa445597db0a13e81233131cfbdf3803ab75
wupinbobo <lamtdse61743@fpt.edu.vn>
2018-04-30 11:57:05 +0700

    Update User, Department Basic Done, View User+ Project fix

--- Web/Angular/src/app/views/team-management/detail-team/detail-team.component.ts
+++ Web/Angular/src/app/views/team-management/detail-team/detail-team.component.ts
@@ -6,9 +6,16 @@ import { User } from 'app/interfaces/user';
 import { Location } from '@angular/common';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ErrorModalComponent,ConfirmModalComponent } from '../../../cmaComponents/modals';
+import { ErrorModalComponent, ConfirmModalComponent } from '../../../cmaComponents/modals';
 import { StoreService } from '../../../services/tree.service';
 import { DataTableDirective } from 'angular-datatables';
+import * as _ from 'lodash';
+import {
+  CommentModalComponent,
+  SelectUsersModalComponent,
+  SelectTeamsModalComponent,
+  SelectMembersModalComponent
+} from '../../../cmaComponents/modals';
 @Component({
   selector: 'app-detail-team',
   templateUrl: './detail-team.component.html',
@@ -20,13 +27,18 @@ export class DetailTeamComponent implements OnInit {
     lengthChange: false,
   };
   teamID: number;
+  containmember: boolean;
   foundTeam: Team;
-  isLoading: boolean;
   managementMode: boolean;
   currentUser: User;
   isPageLoading: boolean;
   foundUser: User;
   users: User[];
+  foundUsers: User[];
+  isLoading: {
+    openAssignMembersModal: boolean
+    openUnAssignMembersModal: boolean
+  };
   constructor(
     private teamService: TeamService,
     private userService: UserService,
@@ -36,11 +48,16 @@ export class DetailTeamComponent implements OnInit {
     private route: ActivatedRoute,
     private location: Location
   ) {
-    this.userService.getAllUser().then(value => {
-      this.users = value;
-    })
+    this.containmember = false;
+    this.isLoading = {
+      openAssignMembersModal: false,
+      openUnAssignMembersModal: false,
+    };
     this.isPageLoading = true;
     this.currentUser = this.storeService.get(['currentUser']);
+    this.userService.getFreeUser().then(value => {
+      this.foundUsers = value;
+    })
   }
 
   ngOnInit() {
@@ -70,6 +87,7 @@ export class DetailTeamComponent implements OnInit {
     this.teamService.getDetail(this.teamID)
       .then(value => {
         this.foundTeam = value;
+        this.users = this.foundTeam.users;
         if (!this.managementMode) {
           this.foundTeam.users = this.foundTeam.users.filter(user => {
             return user.isActive
@@ -107,6 +125,7 @@ export class DetailTeamComponent implements OnInit {
     } else {
       const onConfirm = () => {
         this.teamService.setRole(this.foundTeam.id, this.foundUser.id, true);
+
       }
       const initialState = {
         message: `Are you sure to promote ${this.foundUser.name} to manager?`,
@@ -114,5 +133,98 @@ export class DetailTeamComponent implements OnInit {
       };
       this.modalService.show(ConfirmModalComponent, { initialState, class: 'modal-dialog' });
     }
+  }
+
+
+  handleOnAssignMembersBtnClick() {
+    this.isLoading.openAssignMembersModal = true;
+    const pool = _.filter(this.foundUsers, (user: User) => {
+      if (this.currentUser.isAdmin) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    const onConfirm = (selelectedMembers: User[]) => {
+      let selectedIds = _.map(selelectedMembers, 'id');
+      if (selectedIds.length == 0) {
+        this.containmember = true;
+      }
+      if (!this.containmember) {
+        this.teamService.assignTeam(selectedIds, this.teamID)
+          .then(value => {
+            this.foundTeam = value;
+            this.isLoading.openAssignMembersModal = false
+          })
+          .catch(reason => {
+            this.showErrorModal(reason.Message);
+            this.isLoading.openAssignMembersModal = false
+          })
+      } else {
+        this.showErrorModal('Please select members!');
+        this.isLoading.openAssignMembersModal = false;
+      }
+    };
+
+    const initialState = {
+      confirmCallback: onConfirm,
+      cancelCallback: () => {
+        this.isLoading.openAssignMembersModal = false
+      },
+      closeCallback: () => {
+        this.isLoading.openAssignMembersModal = false
+      },
+      userPool: pool,
+      title: `Assign members`,
+      confirmButtonText: 'Assign'
+    };
+    this.modalService.show(SelectUsersModalComponent, { initialState, class: 'modal-dialog', ignoreBackdropClick: true });
+  }
+
+  handleOnUnAssignMembersBtnClick() {
+    this.isLoading.openUnAssignMembersModal = true;
+    console.debug(this.foundTeam.users.length);
+    const pool = _.filter(this.foundTeam.users, (user: User) => {
+      if (this.currentUser.isAdmin) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    const onConfirm = (selelectedMembers: User[]) => {
+      let selectedIds = _.map(selelectedMembers, 'id');
+      if (selectedIds.length == 0) {
+        this.containmember = true;
+      }
+      if (!this.containmember) {
+        this.teamService.unAssignTeam(selectedIds,this.foundTeam.id)
+          .then(value => {
+            this.foundTeam = value;
+            this.isLoading.openUnAssignMembersModal = false;
+          })
+          .catch(reason => {
+            this.showErrorModal(reason.Message);
+            this.isLoading.openUnAssignMembersModal = false;
+          })
+      } else {
+        this.showErrorModal('Please select members!');
+        this.isLoading.openUnAssignMembersModal = false;
+      }
+    };
+
+    const initialState = {
+      confirmCallback: onConfirm,
+      cancelCallback: () => {
+        this.isLoading.openUnAssignMembersModal = false
+      },
+      closeCallback: () => {
+        this.isLoading.openUnAssignMembersModal = false
+      },
+      userPool: pool,
+      // selectedUsers: selected,
+      title: `Un-Assign`
+    };
+    this.modalService.show(SelectUsersModalComponent, {initialState, class: 'modal-dialog', ignoreBackdropClick: true});
   }
 }
