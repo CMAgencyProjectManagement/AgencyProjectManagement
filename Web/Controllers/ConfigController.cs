d9038c318c0be45f11cbfbf25e94afaaafff7e3e
phatbt <nyxnostalgia@gmail.com>
2018-04-30 12:51:35 +0700

    ConfigController: SetConfigs()
    Config validation

--- Web/Controllers/ConfigController.cs
+++ Web/Controllers/ConfigController.cs
@@ -41,36 +41,73 @@ namespace Web.Controllers
         {
             try
             {
+                bool flag = true;
+                if (configViewModel.PenatyPercent==0)
+                {
+                    ModelState.AddModelError("PenatyPercent", "Penaty percent must not be empty");
+                    flag = false;
+                }
                 if (configViewModel.PenatyPercent < 0 || configViewModel.PenatyPercent > 100)
                 {
                     ModelState.AddModelError("PenatyPercent", "Penaty percent must be between 0 and 100");
+                    flag = false;
                 }
-
+                if (configViewModel.MaxDuration == 0)
+                {
+                    ModelState.AddModelError("MaxDuration", "Max duration must not be empty");
+                    flag = false;
+                }
                 if (configViewModel.MaxDuration < 1)
                 {
                     ModelState.AddModelError("MaxDuration", "Max duration must be larger than 0");
-                }
+                    flag = false;
 
+                }
+                if (configViewModel.LowPoint == 0)
+                {
+                    ModelState.AddModelError("LowPoint", "Low priority point must not be empty");
+                    flag = false;
+                }
                 if (configViewModel.LowPoint < 1)
                 {
                     ModelState.AddModelError("LowPoint", "Low priority point must be larger or equal to 1");
-                }
+                    flag = false;
 
+                }
+                if (configViewModel.MediumPoint == 0)
+                {
+                    ModelState.AddModelError("MediumPoint", "Medium priority point must not be empty");
+                    flag = false;
+                }
                 if (configViewModel.MediumPoint < 1)
                 {
                     ModelState.AddModelError("MediumPoint", "Medium priority point must be larger or equal to 1");
+                    flag = false;
                 }
-
+                if (configViewModel.HighPoint == 0)
+                {
+                    ModelState.AddModelError("HightPoint", "High priority point must not be empty");
+                    flag = false;
+                }
                 if (configViewModel.HighPoint < 1)
                 {
                     ModelState.AddModelError("HightPoint", "High priority point must be larger or equal to 1");
+                    flag = false;
                 }
-
+                if (configViewModel.MinAge == 0)
+                {
+                    ModelState.AddModelError("MinAge", "Min age must not be empty");
+                    flag = false;
+                }
                 //http://www.ilo.org/global/standards/subjects-covered-by-international-labour-standards/child-labour/lang--en/index.htm
                 if (configViewModel.MinAge < 13)
                 {
                     ModelState.AddModelError("MinAge", "Minimun age point must be larger or equal to 13");
+                    flag = false;
                 }
+                if (flag == false)
+                    return Content(HttpStatusCode.BadRequest, ResponseHelper.GetExceptionResponse(ModelState));
+
 
 
                 if (ModelState.IsValid)
