b3ea5df85936e685b37dec9fa9e2124a09ff04f7
phatbt <nyxnostalgia@gmail.com>
2018-05-02 11:42:04 +0700

    ChecklistPart1
    ChecklistPart1

--- Web/Controllers/ChecklistController.cs
+++ Web/Controllers/ChecklistController.cs
@@ -1,9 +1,14 @@
-﻿using System;
+﻿using Entity;
+using Microsoft.AspNet.Identity;
+using Newtonsoft.Json.Linq;
+using Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Http;
+using Web.ViewModels;
 
 namespace Web.Controllers
 {
@@ -16,12 +21,34 @@ namespace Web.Controllers
 
         [HttpPost]
         [Route("")]
-        [Authorize(Roles = "Manager")]
-        public IHttpActionResult CreateChecklist()
+        [System.Web.Http.Authorize(Roles = "Manager")]
+        public IHttpActionResult CreateCheckList(CreateChecklistModel createChecklistModel)
         {
             try
             {
-                throw new NotImplementedException();
+                if (ModelState.IsValid)
+                {
+                    using (CmAgencyEntities db = new CmAgencyEntities())
+                    {
+                        ChecklistService checklistService = new ChecklistService(db);
+                        UserService userService = new UserService(db);
+                        string loginedUserId = User.Identity.GetUserId();
+                        User creator = userService.GetUser(loginedUserId);
+                        CheckList newCheckList = checklistService.CreateChecklist(
+                            createChecklistModel.Name,
+                            (int)createChecklistModel.TaskId,
+                            creator
+                            );
+                        JObject dataObject = checklistService.ParseToJson(newCheckList);
+                        return Ok(ResponseHelper.GetResponse(dataObject));
+                    }
+
+                }
+                else
+                {
+                    return Content(HttpStatusCode.BadRequest,
+                        ResponseHelper.GetExceptionResponse(ModelState));
+                }
             }
             catch (Exception ex)
             {
@@ -29,7 +56,7 @@ namespace Web.Controllers
                     ResponseHelper.GetExceptionResponse(ex));
             }
         }
-        
+
         [HttpDelete]
         [Route("{checkListId:int}")]
         [Authorize(Roles = "Manager")]
@@ -45,7 +72,7 @@ namespace Web.Controllers
                     ResponseHelper.GetExceptionResponse(ex));
             }
         }
-        
+
         [HttpPost]
         [Route("item")]
         [Authorize(Roles = "Manager")]
@@ -61,7 +88,7 @@ namespace Web.Controllers
                     ResponseHelper.GetExceptionResponse(ex));
             }
         }
-        
+
         [HttpDelete]
         [Route("{checkListId:int}/item/{checklistItemId:int}")]
         [Authorize(Roles = "Manager")]
