117242627476385daa019d630fb46841f65080c7
Điền <diendtlse61501@fpt.edu.vn>
2018-05-02 11:45:37 +0700

    thêm trang archive

--- Web/Angular/src/app/views/project/project-management-routing.module.ts
+++ Web/Angular/src/app/views/project/project-management-routing.module.ts
@@ -66,7 +66,7 @@ const routes: Routes = [
         }
       },
       {
-        path: ':id/archive',
+        path: ':id/taskarchive',
         component: ArchiveComponent,
         data: {
           title: 'View archives'
