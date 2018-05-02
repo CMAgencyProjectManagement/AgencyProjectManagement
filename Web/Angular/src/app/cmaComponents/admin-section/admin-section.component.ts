da6999f0d007d8820511df86a67e5baeda1eeb7f
wupinbobo <lamtdse61743@fpt.edu.vn>
2018-05-02 10:43:17 +0700

    Dashboard Project Fix

--- Web/Angular/src/app/cmaComponents/admin-section/admin-section.component.ts
+++ Web/Angular/src/app/cmaComponents/admin-section/admin-section.component.ts
@@ -1,43 +1,41 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild,AfterViewInit } from '@angular/core';
 import { ProjectService } from '../../services/project.service';
 import { Project } from '../../interfaces/project';
 import { User } from '../../interfaces/user';
 import { DataTableDirective } from 'angular-datatables';
 import { UserService } from '../../services/user.service';
+import { Subject } from 'rxjs/Subject';
 @Component({
   selector: 'app-admin-section',
   templateUrl: './admin-section.component.html',
   styleUrls: ['./admin-section.component.scss']
 })
 
-export class AdminSectionComponent implements OnInit {
+export class AdminSectionComponent implements OnInit,AfterViewInit  {
   @ViewChild(DataTableDirective) datatableElement: DataTableDirective;
+  dtElement: DataTableDirective;
   datatableOptions: DataTables.Settings = {
     lengthChange: false,
-    columnDefs: [
-      {
-        searchable: false,
-        orderable: false,
-        targets: [5]
-      }
-    ],
     order: [
-      [4, 'desc']
+      [0, 'desc']
     ]
   };
+  dtTrigger: Subject<any> = new Subject();
   foundProjects: Project[];
   projects: Project[];
   isPageLoading: boolean;
   constructor(private projectService: ProjectService,
     private userService: UserService) {
+    this.projects = [];
     this.projectService.getAllProjects()
       .then(data => {
         this.projects = data;
-        for (let i = 0; i < this.projects.length; i++) {
-          if (this.projects[i].status != 0 && this.projects[i].status!=1){
-            this.foundProjects.push(this.projects[i]);
-          }
-        }
+
+        // for (let i = 0; i < this.projects.length; i++) {
+        //   if (this.projects[i].status != 0 && this.projects[i].status!=1){
+        //     this.foundProjects.push(this.projects[i]);
+        //   }
+        // }
         this.isPageLoading = false;
       })
       .catch(reason => {
@@ -47,9 +45,20 @@ export class AdminSectionComponent implements OnInit {
 
   ngOnInit() {
   }
+  rerender(): void {
+    this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+      // Destroy the table first
+      dtInstance.destroy();
+      // Call the dtTrigger to rerender again
+      this.dtTrigger.next();
+    });
+   }
   search(searchStr: string) {
     this.datatableElement.dtInstance.then(
       (dtInstance: DataTables.Api) => dtInstance.search(searchStr).draw()
     );
+  }
+  ngAfterViewInit(): void {
+    this.dtTrigger.next();
   }
 }
